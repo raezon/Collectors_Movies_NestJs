1237ad0f7fbd80c5520036edb41abfb4850fb007
raezon <amardjebabla10@gmail.com>
2023-03-23 20:05:54 +0100

    Enhance pipeline scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,6 +2,7 @@ cd ..
 cd ..
 cd home/ec2-user/nodejs-aws-codedeploy-pipeline/dist/src
 
+sudo npm i -g pm2
 sudo pm2 stop main.js --name=nodejs-express-a
 sudo pm2 start main.js --name=nodejs-express-a
 
