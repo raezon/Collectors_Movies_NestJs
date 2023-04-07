02d2e98603206c8296ead83d8ff79e9a1679c383
Ammar djebabla <amardjebabla10@gmail.com>
2023-04-07 15:53:42 +0100

    Integration of new aproach for deploying app without a env file pushed

--- exportEnv.sh
+++ exportEnv.sh
@@ -1,17 +1,10 @@
-#!/bin/bash
-
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline
-echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log 
-npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-
-#Getting the branch_name
-BRANCH_NAME=$(cat envfile)
-echo "Starting application with branches: $BRANCH_NAME"
-
 #Begginging env
 sudo touch .env
+BRANCH_NAME=$(curl https://factory-env-files.s3.eu-west-3.amazonaws.com/exported_env_vars.txt)
+echo "Deploying branch: $BRANCH_NAME "
+
+
 PARAMETER_NAME="/${BRANCH_NAME}/DATABASE_URL"
-echo "$PARAMETER_NAME"
 CHECK="/${BRANCH_NAME}/check"
 DATABASE_URL=$(aws ssm get-parameter  --name "$PARAMETER_NAME" --region eu-west-3  --query "Parameter.Value" --output text)
 CHECK=$(aws ssm get-parameter --name "$CHECK" --region eu-west-3 --query "Parameter.Value" --output text)
@@ -21,16 +14,13 @@ JWT_TOKEN_AUDIENCE=$(aws ssm get-parameter --name "JWT_TOKEN_AUDIENCE" --region 
 JWT_TOKEN_ISSUER=$(aws ssm get-parameter --name "JWT_TOKEN_ISSUER" --region eu-west-3 --query "Parameter.Value" --output text)
 echo "$DATABASE_URL"
 # Export the parameter value as an environment variable
- DATABASE_URL="$DATABASE_URL"
- JWT_ACCESS_TOKEN_TTL="$JWT_ACCESS_TOKEN_TTL"
- JWT_SECRET="$JWT_SECRET"
- JWT_TOKEN_AUDIENCE="$JWT_TOKEN_AUDIENCE"
- JWT_TOKEN_ISSUER="$JWT_TOKEN_ISSUER"
- check="$CHECK"
+export DATABASE_URL="$DATABASE_URL"
+export JWT_ACCESS_TOKEN_TTL="$JWT_ACCESS_TOKEN_TTL"
+export JWT_SECRET="$JWT_SECRET"
+export JWT_TOKEN_AUDIENCE="$JWT_TOKEN_AUDIENCE"
+export JWT_TOKEN_ISSUER="$JWT_TOKEN_ISSUER"
+export check="$CHECK"
 sudo rm .env
 printf "DATABASE_URL=%s\nJWT_ACCESS_TOKEN_TTL=%s\nJWT_SECRET=%s\nJWT_TOKEN_AUDIENCE=%s\nJWT_TOKEN_ISSUER=%s\ncheck=%s\n" "$DATABASE_URL" "$JWT_ACCESS_TOKEN_TTL" "$JWT_SECRET" "$JWT_TOKEN_AUDIENCE" "$JWT_TOKEN_ISSUER" "$CHECK" >> .env
 sudo chmod +rw .env
 #Ending env
-npm run build
-
-
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,10 @@
-cd ..
-cd ..
-cd home/ec2-user/nodejs-aws-codedeploy-pipeline/dist/src
 
-sudo npm i -g pm2
+
+cd ..
+cd ..
+cd home/ec2-user/nodejs-aws-codedeploy-pipeline
+
+
 sudo pm2 stop main.js --name=nodejs-express-a
 sudo pm2 start main.js --name=nodejs-express-a
 
