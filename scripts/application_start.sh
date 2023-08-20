782a6d18fd4ef6e2a5a337847a3d8ee7d25b069f
ammar djebabla <47975444+raezon@users.noreply.github.com>
2023-08-20 20:45:12 +0100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,7 @@
-
-
 cd ..
 cd ..
-cd home/ec2-user/nodejs-aws-codedeploy-pipeline
+cd home/ec2-user/nodejs-aws-codedeploy-pipeline/dist/src
 
-npm run start:dev
-#sudo pm2 stop main.js --name=nodejs-express-a
-#sudo pm2 start main.js --name=nodejs-express-a
 
+sudo pm2 stop main.js --name=nodejs-express-a
+sudo pm2 start main.js --name=nodejs-express-a
